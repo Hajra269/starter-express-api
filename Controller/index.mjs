718049c4ad549140089fb82363bb950c606df3c6
unknown <nixaamllc2009@gmail.com>
2023-12-06 17:51:18 +0500

    ahmed

--- Controller/index.mjs
+++ Controller/index.mjs
@@ -10,60 +10,48 @@ const authorizationHeader = `Basic ${Buffer.from(
   `${email}:${apiToken}`
 ).toString("base64")}`;
 
+const fetchOptions = {
+  method: "GET",
+  headers: {
+    Authorization: authorizationHeader,
+    Accept: "application/json",
+  },
+};
+
 const func = async (req, res) => {
   try {
-    // const agent = new https.Agent({
-    //   rejectUnauthorized: false,
-    // });
-
     const projectsResponse = await fetch(
       "https://proprint.atlassian.net/rest/api/3/project",
-      {
-        method: "GET",
-        headers: {
-          Authorization: authorizationHeader,
-          Accept: "application/json",
-        },
-        // agent: agent,
-      }
+      fetchOptions
     );
-
     const projectsData = await projectsResponse.json();
 
-    const projectDetails = [];
-
-    for (const project of projectsData) {
-      const projectResponse = await fetch(
+    const requests = projectsData.map(async (project) => {
+      const projectResponsePromise = fetch(
         `https://proprint.atlassian.net/rest/api/3/search?jql=project=${project.key}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: authorizationHeader,
-            Accept: "application/json",
-          },
-          //agent: agent,
-        }
-      );
-      const projectData = await projectResponse.json();
-      projectDetails.push(projectData);
-    }
-    const allAsignee = [];
-    for (const project of projectsData) {
-      const projectResponse = await fetch(
+        fetchOptions
+      ).then((response) => response.json());
+
+      const assignableUsersResponsePromise = fetch(
         `https://proprint.atlassian.net/rest/api/2/user/assignable/search?project=${project.key}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: authorizationHeader,
-            Accept: "application/json",
-          },
-          //agent: agent,
-        }
-      );
-      const projectData = await projectResponse.json();
-      allAsignee.push(projectData);
-    }
-    res.json({ projectDetails: projectDetails, allAsignee: allAsignee });
+        fetchOptions
+      ).then((response) => response.json());
+
+      const [projectDetails, assignableUsers] = await Promise.all([
+        projectResponsePromise,
+        assignableUsersResponsePromise,
+      ]);
+
+      return { projectDetails, assignableUsers };
+      // return { projectDetails };
+    });
+
+    const results = await Promise.all(requests);
+
+    res.json({
+      projectDetails: results.map((result) => result.projectDetails),
+      allAsignee: results.map((result) => result.assignableUsers),
+    });
   } catch (error) {
     console.error("Error fetching project details:", error);
     res.status(500).json({ error: "Error fetching project details" });
@@ -147,4 +135,84 @@ const searchByAssignee = async (req, res) => {
   }
 };
 
-export { func, search, searchByAssignee };
+const groupByFilters = async (req, res) => {
+  try {
+    const { query } = req.query;
+    const jqlQuery = `issuetype = "${query}"`;
+    const searchResponse = await fetch(
+      `https://proprint.atlassian.net/rest/api/2/search?jql=${encodeURIComponent(
+        jqlQuery
+      )}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: authorizationHeader,
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const FilteredData = await searchResponse.json();
+
+    return res.json(FilteredData);
+  } catch (error) {
+    console.error("Error searching Jira:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// const groupByFilters = async (req, res) => {
+//   try {
+//     const { query } = req.query;
+//     let startAt = 0;
+//     const maxResults = 50; // Set your desired maxResults value
+
+//     let allFilteredData = [];
+
+//     while (true) {
+//       const jqlQuery = `issuetype = "${query}"`;
+//       const searchResponse = await fetch(
+//         `https://proprint.atlassian.net/rest/api/2/search?jql=${encodeURIComponent(
+//           jqlQuery
+//         )}&startAt=${startAt}&maxResults=${maxResults}`,
+//         {
+//           method: "GET",
+//           headers: {
+//             Authorization: authorizationHeader,
+//             Accept: "application/json",
+//           },
+//         }
+//       );
+
+//       if (!searchResponse.ok) {
+//         const errorText = await searchResponse.text();
+//         console.error(
+//           "Error searching Jira:",
+//           searchResponse.status,
+//           errorText
+//         );
+//         return res.status(searchResponse.status).json({ error: errorText });
+//       }
+
+//       const filteredData = await searchResponse.json();
+
+//       if (filteredData.issues.length === 0) {
+//         // No more issues, break the loop
+//         break;
+//       }
+
+//       // Concatenate the current page of issues to the result array
+//       allFilteredData = allFilteredData.concat(filteredData.issues);
+
+//       // Update the startAt for the next page
+//       startAt += maxResults;
+//     }
+
+//     return res.json(allFilteredData);
+//   } catch (error) {
+//     console.error("Error searching Jira:", error);
+//     return res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+export { func, search, searchByAssignee, groupByFilters };
--- server.mjs
+++ server.mjs
@@ -97,6 +97,7 @@ app.use(cors());
 app.use("/api/project-data", projectRouter);
 app.use("/api/project-search", projectRouter);
 app.use("/api/project-assigne", projectRouter);
+app.use("/api/project-groupByfilter", projectRouter);
 
 const port = 5000;
 app.listen(port, () => {
